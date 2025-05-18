f45ab3d20b335edeabca043b0b8db8dbc8ee2091
DreamerAmit <amitmahajan274@outlook.com>
2025-05-18 17:51:06 +0530

    Login Page Branding

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -67,7 +67,7 @@ const Login: React.FC = () => {
             variant="subtitle1" 
             sx={{ mb: 3, textAlign: 'center', color: 'text.secondary' }}
           >
-            Your Contract Management Solution
+           Track your renewals efficiently
           </Typography>
 
           <Avatar sx={{ bgcolor: 'primary.main' }}>
